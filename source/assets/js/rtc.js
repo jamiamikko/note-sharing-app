9c1aa2db9eaf968e78e3abbbac5f70144140d3c9
jamiamikko <jamiamikko@gmail.com>
2018-11-29 20:52:52 +0200

    Code cleanup

--- source/assets/js/rtc.js
+++ source/assets/js/rtc.js
@@ -18,36 +18,7 @@ const $chatBox = document.querySelector('.js-chat-window');
 const $chatInput = document.querySelector('.js-input');
 const $form = document.querySelector('.js-chat-form');
 
-const caller = new RTCPeerConnection(servers);
-
-const onIceCandidate = (evt) => {
-  socket.emit('candidate', JSON.stringify({candidate: evt.candidate}));
-};
-
-caller.onaddstream = (event) => {
-  console.log('onaddstream called');
-};
-
-caller.onicecandidate = (evt) => {
-  if (!evt.candidate) return;
-  console.log('onicecandidate called');
-  onIceCandidate(evt);
-};
-
 const socket = io.connect('http://localhost:3000');
-
-const makeNewCall = (socket) => {
-  caller
-    .createOffer()
-    .then((res) => {
-      caller.setLocalDescription(new RTCSessionDescription(res));
-
-      socket.emit('call', JSON.stringify(res));
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-};
 
 $form.addEventListener(
   'submit',
@@ -72,25 +43,4 @@ socket.on('message', (message) => {
     '<div class="chat__row"><p class="chat__sender">User:</p><p class="chat__comment">"' +
     message +
     '"</p></div>';
-});
-
-socket.on('answer', (message) => {
-  caller.setRemoteDescription(new RTCSessionDescription(JSON.parse(message)));
-});
-
-socket.on('call', (message) => {
-  console.log(message);
-  socket.emit('answer', 'Call answered');
-
-  caller.setRemoteDescription(new RTCSessionDescription(JSON.parse(message)));
-
-  caller.createAnswer().then((call) => {
-    caller.setLocalDescription(new RTCSessionDescription(call));
-    socket.emit('answer', JSON.stringify(call));
-  });
-});
-
-socket.on('candidate', (message) => {
-  console.log(message);
-  caller.addIceCandidate(new RTCIceCandidate(JSON.parse(message).candidate));
 });
