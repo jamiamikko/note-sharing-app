64141fa1f63b0205a076a4289948484776637fda
jamiamikko <jamiamikko@gmail.com>
2018-12-02 19:38:26 +0200

    Set env variable

--- server.js
+++ server.js
@@ -51,6 +51,21 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+console.log(process.env.IS_PROD);
+
+if (process.env.IS_PROD === true) {
+  app.enable('trust proxy');
+
+  app.use((req, res, next) => {
+    if (req.secure) {
+      // request was via https, so do no special handling
+      next();
+    } else {
+      // request was via http, so redirect to https
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+  });
+}
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
