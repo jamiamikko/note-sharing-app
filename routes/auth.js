a37a24463a96e15a7055e017c733328a0f623d3e
jamiamikko <jamiamikko@gmail.com>
2018-12-06 19:41:40 +0200

    Update docs

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,26 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (passport) => {
+  /**
+   * @api {post} / Authenticate user
+   * @apiName authenticate-user
+   * @apiGroup Users
+   *
+   * @apiDescription Endpoint for user authentication.
+   *
+   *
+   * @apiHeader {String} Content-type  application/x-www-form-urlencoded
+   *
+   * @apiParamExample Request-Example
+   *
+   * username=user&password=password
+   *
+   * @apiSuccessExample Success-Response:
+   *    302 Redirect to index
+   *
+   * @apiErrorExample Error-Response:
+   *    302 Redirect back to login
+   */
   return router.post(
     '/',
     passport.authenticate('local', {
